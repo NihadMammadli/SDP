e831f091ce7f4effc26d07b974a826f0bdd16e16
Nihad Mammadli <nihadm03@gmail.com>
2024-02-21 00:43:19 +0400

    some more advancements

--- Back/NodeJS/src/socket.js
+++ Back/NodeJS/src/socket.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 const { exec } = require('child_process');
+const fs = require('fs');
 
 const client = new Client({
     user: 'cmsuser',
@@ -14,7 +15,6 @@ let lastTimestamp = null;
 client.connect()
     .then(() => {
         console.log('Connected to PostgreSQL');
-
         client.query('LISTEN submissions;');
 
         setInterval(() => {
@@ -39,15 +39,25 @@ async function handleNotification(payload) {
         if (result.rows && result.rows.length > 0) {
             const lastRow = result.rows[result.rows.length - 1];
             if (!lastTimestamp || lastRow.timestamp > lastTimestamp) {
-                console.log(lastRow);
                 lastTimestamp = lastRow.timestamp;
-                
+
+                const output = {
+                    task_id: lastRow.task_id,
+                    participation_id: lastRow.participation_id,
+                    submission_id: lastRow.id,
+                    code: '',
+                    submission_date: lastRow.timestamp
+                };
+
                 exec(`python3 /home/nihad/Desktop/Projects/SDP/Back/Python/downloader.py ${lastRow.id}`, (error, stdout, stderr) => {
                     if (error) {
                         console.error('Error executing Python script:', error);
                         return;
                     }
-                    console.log('Python script output:', stdout);
+
+                    output.code = stdout.trim();
+
+                    updateOrCreateJson(output);
                 });
             } else {
                 console.log("No new data.");
@@ -58,4 +68,40 @@ async function handleNotification(payload) {
     } catch (error) {
         console.error('Error handling notification:', error);
     }
+}
+
+function updateOrCreateJson(data) {
+    fs.readFile('output.json', (err, jsonString) => {
+        if (err) {
+            console.error('Error reading JSON file:', err);
+            return;
+        }
+        
+        let json = {};
+        if (jsonString) {
+            json = JSON.parse(jsonString);
+        }
+        
+        const { task_id, participation_id, submission_id, code, submission_date } = data;
+        
+        if (!json[task_id]) {
+            json[task_id] = {};
+        }
+        
+        delete json[task_id][participation_id];
+        
+a        json[task_id][participation_id] = [{
+            submission_id,
+            code,
+            submission_date
+        }];
+        
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), (err) => {
+            if (err) {
+                console.error('Error writing JSON file', err);
+            } else {
+                console.log('JSON file updated successfully');
+            }
+        });
+    });
 }
