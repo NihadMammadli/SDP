ec8b1eb8bc2cc4d6c7acd6e02be6b0ae457dc29c
Nihad Mammadli <nihadm03@gmail.com>
2024-02-28 12:46:11 +0400

    Front updated

--- Back/NodeJS/src/data.js
+++ Back/NodeJS/src/data.js
@@ -3,9 +3,10 @@ const { Client } = require('pg');
 const fs = require('fs').promises;
 const path = require('path');
 const cors = require('cors');
+const submissions = require('./output.json');
 
 const app = express();
-const port = 9999;
+const port = 10000;
 
 const dbConfig = {
     user: 'cmsuser',
@@ -19,62 +20,68 @@ const createClient = () => new Client(dbConfig);
 
 app.use(cors());
 
-app.get('/api/users', async (req, res) => {
+app.get('/cms/sections', async (req, res) => {
     const client = createClient();
 
     try {
         await client.connect();
 
-        const usersQuery = 'SELECT * FROM users';
-        const usersResult = await client.query(usersQuery);
-        const users = usersResult.rows;
+        const query = `
+            SELECT s.section, json_agg(u) as users
+            FROM sections s
+            LEFT JOIN users u ON s.user_id = u.id
+            GROUP BY s.section
+            ORDER BY s.section
+        `;
 
-        const comparisonResultsPath = '/home/nihad/Desktop/Projects/cmsBack/data/json/comparison_results.json';
-        const comparisonResults = await fs.readFile(comparisonResultsPath, 'utf-8');
-        const comparisonResultsJson = JSON.parse(comparisonResults);
+        const result = await client.query(query);
 
-        for (const user of users) {
-            const submissionsQuery = `SELECT id FROM submissions WHERE participation_id = ${user.id} ORDER BY timestamp DESC LIMIT 1`;
-            const submissionsResult = await client.query(submissionsQuery);
-            user.submissions = [];
+        const sectionsWithoutPasswords = result.rows.map(section => ({
+            section: section.section,
+            users: section.users.map(user => {
+                
+                const userSubmissions = Object.values(submissions).flatMap(taskSubmissions =>
+                    taskSubmissions[user.id] || []
+                );
 
-            for (const submission of submissionsResult.rows) {
-                const submissionFilePath = `/home/nihad/Desktop/Projects/cmsBack/data/codes/${user.id}_${submission.id}.cpp`;
+                return {
+                    id: user.id,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    username: user.username,
+                    email: user.email,
+                    timezone: user.timezone,
+                    submissions: userSubmissions
+                };
+            })
+        }));
 
-                try {
-                    const content = await fs.readFile(submissionFilePath, 'utf-8');
 
-                    const comparisonKey = `${user.id}_${submission.id}`;
-                    const similarityInfo = comparisonResultsJson[comparisonKey];
+        res.json(sectionsWithoutPasswords);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        await client.end();
+    }
+});
 
-                    const submissionInfo = {
-                        id: submission.id,
-                        content,
-                        files: [],
-                    };
+app.get('/cms/comparisons/:id', async (req, res) => {
+    const { id } = req.params;
+    const client = createClient();
 
-                    for (const fileKey in similarityInfo) {
-                        const fileSimilarityInfo = similarityInfo[fileKey];
-                        submissionInfo.files.push({
-                            fileId: fileKey,
-                            similarity: fileSimilarityInfo ? fileSimilarityInfo.similarity : null,
-                            commonLines: fileSimilarityInfo ? fileSimilarityInfo.common_lines : null,
-                        });
-                    }
+    try {
+        await client.connect();
 
-                    user.submissions.push(submissionInfo);
-                } catch (readFileError) {
-                    console.error(`Error reading file ${submissionFilePath}:`, readFileError);
-                    user.submissions.push({
-                        id: submission.id,
-                        content: 'File not found or could not be read.',
-                        files: [],
-                    });
-                }
-            }
-        }
+        const query = `
+        SELECT *
+        FROM comparisons
+        WHERE (submission_id = $1 OR compared_contestant_id = $1) AND status = 1
+    `;
 
-        res.json({ users });
+        const result = await client.query(query, [id]);
+
+        res.json(result.rows);
     } catch (err) {
         console.error('Error:', err);
         res.status(500).json({ error: 'Internal Server Error' });
