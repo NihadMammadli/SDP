f2f5773e6011bd47aea6e099dcb2a3ea5cf0020d
Nihad Mammadli <nihadm03@gmail.com>
2024-03-27 02:14:29 +0400

    More changes

--- Front/src/Pages/Seats/index.jsx
+++ Front/src/Pages/Seats/index.jsx
@@ -54,6 +54,7 @@ function App() {
 
     socket.on('output', data => {
       console.log(data);
+      messageApi.success(`User with id ${data?.participation_id} submitted`);
       setLastSubmitted(data)
       setTimeout(fetchSections, 5000);
     });
@@ -78,39 +79,65 @@ function App() {
   return (
     <>
       {contextHolder}
-      <Row gutter={[16, 16]}>
-        {sections?.map(section => (
-          <Col key={section?.id} style={{ border: "1px solid black", borderRadius: "6px", padding: "5px", margin: "auto" }}>
-            <Row style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-              <Typography.Title level={3}>Section: {section?.section.toUpperCase()}</Typography.Title>
-            </Row>
-            <Row gutter={[16, 16]}>
-              {section?.users.map(user => (
-                <Col key={user?.id} span={8} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                  <div
-                    // onClick={(_) => showView(user)}
-                    style={{
-                      width: '100px',
-                      borderRadius: "5px",
-                      border: "0.5px solid black",
-                      height: '100px',
-                      backgroundColor: user?.id === lastSubmitted?.participation_id ? (user?.id === lastSimilarityAlarm ? 'red' : (user?.id === lastScoringPreceding ? 'yellow' : (blink ? 'green' : 'white'))) : 'white',
-                      display: 'flex',
-                      flexDirection: "column",
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      transition: 'background-color 0.5s ease'
-                    }}>
-                    <Typography.Text style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{user?.first_name}</Typography.Text>
-                    <Typography.Text style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{user?.last_name}</Typography.Text>
-                  </div>
-                </Col>
-              ))}
-            </Row>
-          </Col>
-        ))}
-      </Row>
-
+      <div style={{ height: '90%' }}>
+        <Row gutter={[16, 16]} style={{ padding: "5px" }}>
+          {sections?.map(section => (
+            <Col key={section?.id} span={5}
+              style={{
+                boxShadow: "0 5px 6px rgba(0, 0, 5, 0.1)",
+                transition: "border-color 0.3s ease",
+                border: "1px solid transparent",
+                borderRadius: "6px",
+                padding: "10px 20px 20px 20px",
+                margin: "auto"
+              }}
+              onMouseEnter={(e) => {
+                e.currentTarget.style.borderColor = "#1677FF";
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.style.borderColor = "transparent";
+              }}
+            >
+              <Row style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <Typography style={{ fontSize: "20px", fontWeight: "600" }}>{section?.section.toUpperCase()}</Typography>
+              </Row>
+              <Row gutter={[16, 16]}>
+                {section?.users.map(user => (
+                  <Col key={user?.id} span={8}
+                    style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    <div
+                      // onClick={(_) => showView(user)}
+                      style={{
+                        width: '100px',
+                        borderRadius: "5px",
+                        height: '100px',
+                        backgroundColor: user?.id === lastSubmitted?.participation_id ? (user?.id === lastSimilarityAlarm ? 'red' : (user?.id === lastScoringPreceding ? 'yellow' : (blink ? 'green' : 'white'))) : 'white',
+                        display: 'flex',
+                        transition: "border-color 0.3s ease",
+                        border: "1px solid transparent",
+                        flexDirection: "column",
+                        boxShadow: "0 5px 6px rgba(15, 15, 15, 0.1)",
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        transition: 'background-color 0.5s ease'
+                      }}
+                      onMouseEnter={(e) => {
+                        e.currentTarget.style.borderColor = "#1677FF";
+                      }}
+                      onMouseLeave={(e) => {
+                        e.currentTarget.style.borderColor = "transparent";
+                      }}
+                    >
+                      <Typography.Text style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{user?.first_name}</Typography.Text>
+                      <Typography.Text style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{user?.last_name}</Typography.Text>
+                    </div>
+                  </Col>
+                ))}
+              </Row>
+            </Col>
+          ))}
+        </Row>
+      </div>
       <View open={viewOpen} close={viewClose} data={viewData} />
     </>
   );
